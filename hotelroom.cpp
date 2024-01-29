56aed46ee9a81689416e212759fe5dc47a2fc614
munib <munib@gmail.com>
2024-01-29 12:29:51 +0500

    main class

---
+++ hotelroom.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+class hotelroom{
+public:
+string customerName;
+string roomNo;
+float payment;
+string roomType
+
+void searchHotels()
+{
+    
+}
+  
+void viewHotels()
+{
+   
+}
+
+void manageRooms()
+{
+  
+}
+
+void showDetails()
+{
+    
+}
+
+ 
+}
+int main() {
+
+    return 0;
+}
